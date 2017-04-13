3c5737ba82a1477575b90cefb0f605aaace93292
student <student@DESKTOP-QJS2RDT>
2017-04-13 09:31:27 -0700

    Add project files.

---
+++ ChessWebApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChessWebApplication.MvcApplication" Language="C#" %>
---
+++ ChessWebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChessWebApplication.Startup))]
+namespace ChessWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
