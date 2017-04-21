080fffff7c11dd7100ada75ce032e21aeb05040e
amjad <aallobadi@lcmail.lcsc.edu>
2017-04-21 10:59:32 -0700

    adding controller and model Teams

---
+++ ChessWebApplication/Models/Team.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ChessWebApplication.Models
+{
+    public class Team
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Totalscore { get; set; }
+    }
+
+    public class TeamDBContext : DbContext
+    {
+        public DbSet<Team> Team { get; set; }
+    }
+}
