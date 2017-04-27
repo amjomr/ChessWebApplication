2fa7ba6be32907eaf347acbd197981c49aa37618
Amjad <aallobadi@lcmail.lcsc.edu>
2017-04-27 10:03:53 -0700

    Adding database and schedule Model

---
+++ ChessWebApplication/Models/Schedule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ChessWebApplication.Models
+{
+    public class Schedule
+    {
+      
+
+        [Key]
+        public int Id { get; set; }
+
+        public string Team1Id { get; set; }
+
+        public string Team2Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+    }
+}
