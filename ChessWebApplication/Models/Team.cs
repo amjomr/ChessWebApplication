bcfc0bccd52a8381e4161926f38a57683acedcbd
Ethan Simpson <ethan_simpson@live.com>
2017-05-01 20:14:36 -0700

    Fixed a few things that I broke

--- ChessWebApplication/Models/Team.cs
+++ ChessWebApplication/Models/Team.cs
@@ -4,15 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Data.Entity;
 
-namespace ChessWebApplication.Models
-{
+
     public class Team
     {
         public int ID { get; set; }
         public string Name { get; set; }
         public int Totalscore { get; set; }
 
-        public List<Schedule> Schedules { get; set; }
+        public List<ChessWebApplication.Models.Schedule> Schedules { get; set; }
     }
 
-}
+    public class TeamDBContext : DbContext
+    {
+        public DbSet<Team> Team { get; set; }
+
+        public System.Data.Entity.DbSet<ChessWebApplication.Models.Schedule> Schedules { get; set; }
+    }
